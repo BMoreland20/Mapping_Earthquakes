fac8d3243eb8a512380901ff4280c8133c591b6c
Blaine Moreland <blaine.l.moreland@gmail.com>
2022-05-26 20:51:21 -0500

    Updates to style and logic

--- Earthquakes_past7days/static/js/logic.js
+++ Earthquakes_past7days/static/js/logic.js
@@ -40,6 +40,36 @@ let overlays = {
   Earthquakes: earthquakes
 };
 
+// Create a legend control object.
+let legend = L.control({
+  position: "bottomright"
+});
+
+// Then add all the details for the legend.
+legend.onAdd = function() {
+  let div = L.DomUtil.create("div", "info legend");
+  const magnitudes = [0, 1, 2, 3, 4, 5];
+  const colors = [
+    "#98ee00",
+    "#d4ee00",
+    "#eecc00",
+    "#ee9c00",
+    "#ea822c",
+    "#ea2c2c"
+  ];
+  
+// Looping through our intervals to generate a label with a colored square for each interval.
+for (var i = 0; i < magnitudes.length; i++) {
+  console.log(colors[i]);
+  div.innerHTML +=
+    "<i style='background: " + colors[i] + "'></i> " +
+    magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+}
+return div;
+};
+
+legend.addTo(map);
+
 // Pass our map layers into our layers control and add the layers control to the map.
 // Then we add a control to the map that will allow the user to change
 // which layers are visible.
